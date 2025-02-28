2c3966f11a40cebb3ff8cfeb6def0d142d81db00
Hikikomori-Kim <gsb1028@naver.com>
2025-02-28 16:47:15 +0900

    dto작성

--- academy/src/main/java/com/kh/academy/dto/CompanyDto.java
+++ academy/src/main/java/com/kh/academy/dto/CompanyDto.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
-public class CompanyDto {
+public class CompanyDto {//
 	private int companyNo; //기업번호
 	private String companyName; //기업명(상호명)
 	private int companyCrNumber;//사업자번호
