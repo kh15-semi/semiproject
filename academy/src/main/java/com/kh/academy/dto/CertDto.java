f4023a0d9bd146733b6354ae3ac909e9ae49fc70
Hikikomori-Kim <gsb1028@naver.com>
2025-03-04 15:15:15 +0900

    이메일 인증관련 추가

---
+++ academy/src/main/java/com/kh/academy/dto/CertDto.java
@@ -0,0 +1,18 @@
+package com.kh.academy.dto;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class CertDto {
+	
+	private String certEmail;
+	private String certNumber;
+	private Timestamp certTime;
+	private Timestamp certConfirm;
+	}
+
---
+++ academy/src/main/java/com/kh/academy/mapper/CertMapper.java
@@ -0,0 +1,27 @@
+package com.kh.academy.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.kh.academy.dto.CertDto;
+
+
+
+@Component
+public class CertMapper implements RowMapper<CertDto>{
+
+	@Override
+	public CertDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		return CertDto.builder()
+				.certEmail(rs.getString("cert_email"))
+				.certNumber(rs.getString("cert_number"))
+				.certTime(rs.getTimestamp("cert_time"))
+				.certConfirm(rs.getTimestamp("cert_confirm"))
+				.build();
+	}
+	
+}
