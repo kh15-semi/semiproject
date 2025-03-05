bc33c9ff772a9004cac05d33c633342be09c362a
Hikikomori-Kim <gsb1028@naver.com>
2025-03-05 13:38:17 +0900

    companyDao

--- academy/src/main/java/com/kh/academy/dao/CompanyDao.java
+++ academy/src/main/java/com/kh/academy/dao/CompanyDao.java
@@ -1,11 +1,12 @@
 package com.kh.academy.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.academy.dto.CompanyDto;
-import com.kh.academy.dto.ReplyDto;
 import com.kh.academy.mapper.CompanyMapper;
 
 @Repository
@@ -15,7 +16,7 @@ public class CompanyDao {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	public void insertCompany(CompanyDto companyDto) { //추가 
+	public void insert(CompanyDto companyDto) { //추가 
 		String sql ="insert into company(company_no, company_name, company_url, "
 				+ "company_contact, company_industry, company_job, company_post, "
 				+ "company_address1, company_address2, company_cr_number) "
@@ -26,21 +27,32 @@ public class CompanyDao {
 		jdbcTemplate.update(sql,data);
 	}
 	
-	public boolean deleteCompany(int companyNo) { //삭제
+	public boolean delete(int companyNo) { //삭제
 		String sql = "delete company where company_no = ?";
 		Object[] data = {companyNo};
 		return jdbcTemplate.update(sql, data) > 0;
 	}		
 	
-	public boolean update(ReplyDto replyDto) { //업데이트
+	public boolean update(CompanyDto companyDto) { //업데이트
 		String sql = "update company "
 				+ "set "
-				+ "company_";
+				+ "company_name=?, company_url=?, company_contact=?, company_industry=?, "
+				+ "company_job=?, company_post=?, company_address1=?, company_address2=?, company_cr_number=? "
+				+ "where company_no=?";
 		Object[] data = {
-				replyDto.getReplyContent(),
-				replyDto.getReplyNo()
+				companyDto.getCompanyName(),companyDto.getCompanyUrl(),companyDto.getCompanyContact(),companyDto.getCompanyIndustry(),
+				companyDto.getCompanyJop(),companyDto.getCompanyPost(),companyDto.getCompanyAddress1(),companyDto.getCompanyAddress2(),
+				companyDto.getCompanyCrNumber()
 		};
 		return jdbcTemplate.update(sql, data) > 0;
+	}	
+	
+	//상세조회 기능
+	public CompanyDto selectOne(int companyNo) {
+		String sql = "select * from company where company_no=?";
+		Object[] data = {companyNo};
+		List<CompanyDto> list = jdbcTemplate.query(sql,companyMapper, data);
+		return list.isEmpty() ? null : list.get(0);
 	}	
 	
 }
